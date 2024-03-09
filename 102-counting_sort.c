2401c097b4f6761c32aa30bf6a14dc8407a222bb
Elyageen <yagosalih452@gmail.com>
2024-03-10 01:33:10 +0300

    task adv 003

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -12,58 +12,61 @@ void counting_sort(int *array, size_t size)
 {
     size_t max = 0;
     size_t i;
-    int *count = malloc((max + 1) * sizeof(int));
-    int *temp = malloc(size * sizeof(int));
-
+    int *count;
+    int *temp;
+    size_t j = 0;
+    
     if (array == NULL || size < 2)
         return;
 
     /* Find the maximum element in the array*/
-    for (i = 0; i < size; i++)
+    max = (size_t)array[0];
+    for (i = 1; i < size; i++)
     {
         if ((size_t)array[i] > max)
             max = (size_t)array[i];
     }
 
-    /* Create a counting array of size max+1 and initialize all elements to 0*/
+    /* Create a counting array of size max + 1 and initialize all elements to 0 */
+    count = malloc((max + 1) * sizeof(int));
     if (count == NULL)
         return;
 
     for (i = 0; i <= max; i++)
         count[i] = 0;
 
-    /* Count the occurrences of each element in the array*/
+    /* Count the occurrences of each element in the array */
     for (i = 0; i < size; i++)
         count[array[i]]++;
 
-    /* Print the counting array*/
+    /* Print the counting array */
     printf("Counting array: ");
     for (i = 0; i <= max; i++)
         printf("%d%s", count[i], i == max ? "\n" : ", ");
 
-    /* Modify the counting array to store the cumulative frequencies*/
-    for (i = 1; i <= max; i++)
-        count[i] += count[i - 1];
-
-    /* Create a temporary array to store the sorted elements*/
+    /* Sort the array based on the counting array */
+    temp = malloc(size * sizeof(int));
     if (temp == NULL)
     {
         free(count);
         return;
     }
 
-    /* Sort the array based on the counting array*/
-    for (i = size - 1; i < size; i--)
+    for (i = 0; i < size; i++)
+        temp[i] = 0;
+
+    
+    for (i = 0; i <= max; i++)
     {
-        temp[count[array[i]] - 1] = array[i];
-        count[array[i]]--;
+        while (count[i]--)
+            temp[j++] = i;
     }
 
-    /* Copy the sorted elements from the temporary array to the original array*/
+    /* Copy the sorted elements from the temporary array to the original array */
     for (i = 0; i < size; i++)
         array[i] = temp[i];
 
-    /* Free the allocated memory*/
+    /* Free the allocated memory */
     free(temp);
     free(count);
 }
