4946a3146909a831ecdca562b9680d7945db0185
Elyageen <yagosalih452@gmail.com>
2024-03-10 01:20:18 +0300

    task adv 002

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 int partition(int *array, int low, int high);
 void quicksort(int *array, int low, int high);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void cocktail_sort_list(listint_t **list);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
