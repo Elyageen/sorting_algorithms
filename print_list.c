d219743df066809e76114269206c33e0cf4236af
Elyageen <yagosalih452@gmail.com>
2024-03-10 00:38:30 +0300

    task 001-1

---
+++ print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+* print_list - Prints a list of integers
+*
+* @list: The list to be printed
+*/
+void print_list(const listint_t *list)
+{
+	const listint_t *current = list;
+
+	while (current != NULL)
+	{
+		printf("%d", current->n);
+		if (current->next != NULL)
+			printf(", ");
+
+		current = current->next;
+	}
+	printf("\n");
+}
--- sort.h
+++ sort.h
@@ -3,17 +3,27 @@
 
 #include <stddef.h>
 
-/* Struct for doubly linked list node */
+/**
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 typedef struct listint_s
 {
-    const int n;                /* Integer stored in the node */
-    struct listint_s *prev;    /* Pointer to the previous element of the list */
-    struct listint_s *next;    /* Pointer to the next element of the list */
+	const int n;
+
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Function prototypes */
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
 void bubble_sort(int *array, size_t size);
+
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
